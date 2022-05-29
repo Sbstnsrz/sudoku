3ab0fc32911f515e0ca8dbd9406a997e5351dafa
Seb <sbstn1@gmail.com>
2022-05-29 01:29:51 -0300

    numbersGenerator prototype

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ var y = scr.height;
 draw.fillStyle = "WhiteSmoke";
 draw.fillRect(0,0,100*xUnit,100*xUnit);
 
-//draw.clearRect(0, 0, xUnit*100, y);
-//draw.beginPath();
 draw.lineCap = "round";
 draw.strokeStyle = "black";
 
+
+//Grey backgrounds:
 draw.fillStyle = "LightGrey";
-draw.fillRect((1)*xUnit, (1)*xUnit, 98/3*xUnit, 98/3*xUnit);
-draw.fillRect((1)*xUnit, (1+98/3*2)*xUnit, 98/3*xUnit, 98/3*xUnit);
-draw.fillRect((1+98/3)*xUnit, (1+98/3)*xUnit, 98/3*xUnit, 98/3*xUnit);
-draw.fillRect((1+98/3*2)*xUnit, (1)*xUnit, 98/3*xUnit, 98/3*xUnit);
+draw.fillRect(           xUnit,            xUnit, 98/3*xUnit, 98/3*xUnit);
+draw.fillRect(           xUnit, (1+98/3*2)*xUnit, 98/3*xUnit, 98/3*xUnit);
+draw.fillRect(  (1+98/3)*xUnit,   (1+98/3)*xUnit, 98/3*xUnit, 98/3*xUnit);
+draw.fillRect((1+98/3*2)*xUnit,            xUnit, 98/3*xUnit, 98/3*xUnit);
 draw.fillRect((1+98/3*2)*xUnit, (1+98/3*2)*xUnit, 98/3*xUnit, 98/3*xUnit);
 
 //draw gameboard lines:
@@ -49,9 +49,23 @@ draw.fillStyle = "black";
 draw.lineWidth = 0.5*xUnit;
 draw.font = "4em Arial";
 for (var y = 0; y < 9; y++){
-    for(var i = 0; i < 9; i++){
-        draw.fillText(i+1, (4.5 + i*(98/9))*xUnit, ((98/9)*(y+1)-1)*xUnit);
+    for(var x = 0; x < 9; x++){
+        draw.fillText(x+1, (4.5 + x*(98/9))*xUnit, ((98/9)*(y+1)-1)*xUnit);
     }
 }
 
+function numbersGenerator(){
+    var temp = [];
+    var randomSort = [];
+    for(var i=1; i<10; i++){ temp.push(i);}
 
+    while(temp.length){
+        var randomPic = Math.floor(Math.random()*temp.length);
+        randomSort.push(temp[randomPic]);
+        temp.splice(randomPic,1);
+    }
+
+    return console.log(randomSort);
+}
+
+numbersGenerator();
