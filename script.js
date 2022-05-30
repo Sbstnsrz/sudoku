58810a6fa3328d2fe94511d3ff2b55a09511c211
Seb <sbstn1@gmail.com>
2022-05-30 01:09:24 -0300

    30-05-22: uncomplete generator

--- script.js
+++ script.js
@@ -45,27 +45,60 @@ do{
 }while(!start);
 
 
-draw.fillStyle = "black";
-draw.lineWidth = 0.5*xUnit;
-draw.font = "4em Arial";
-for (var y = 0; y < 9; y++){
-    for(var x = 0; x < 9; x++){
-        draw.fillText(x+1, (4.5 + x*(98/9))*xUnit, ((98/9)*(y+1)-1)*xUnit);
-    }
-}
-
 function numbersGenerator(){
-    var temp = [];
-    var randomSort = [];
-    for(var i=1; i<10; i++){ temp.push(i);}
+    
+    var matrix = [];
 
-    while(temp.length){
-        var randomPic = Math.floor(Math.random()*temp.length);
-        randomSort.push(temp[randomPic]);
-        temp.splice(randomPic,1);
+    for(var j=0; j<9; j++){
+        var randomLine = [];
+        var numbersLine = [];
+
+        for(var num=1; num<10; num++){ numbersLine.push(num);}
+        while(numbersLine.length){
+           
+            if(j>0){
+                var noList = [];
+                for(var i=0; i<numbersLine.length; i++){
+                    noList.push(numbersLine[i]);
+                }
+
+                for(var col=0; col<j; col++){
+                    var row = randomLine.length;
+                    if(noList.includes(matrix[col][row])){
+                        noList.splice(noList.indexOf(matrix[col][row]), 1);
+                    }
+                }
+                console.log(noList);
+
+                var randomPic=0;
+                if(noList.length>0){
+                    randomPic = Math.floor(Math.random()*noList.length);
+                }
+                randomLine.push(noList[randomPic]);
+                numbersLine.splice(numbersLine.indexOf(noList[randomPic]), 1);
+            }else{
+                var randomPic = Math.floor(Math.random()*numbersLine.length);
+                randomLine.push(numbersLine[randomPic]);
+                numbersLine.splice(randomPic,1);
+            }
+
+        }
+        matrix.push(randomLine);
+        
     }
 
-    return console.log(randomSort);
+    return matrix;
 }
+( ()=>{
+    var matrix = numbersGenerator();
+    draw.fillStyle = "black";
+    draw.lineWidth = 0.5*xUnit;
+    draw.font = "4em Arial";
+    for (var y = 0; y < 9; y++){
+        for(var x = 0; x < 9; x++){
+            draw.fillText(matrix[y][x], (4.5 + x*(98/9))*xUnit, ((98/9)*(y+1)-1)*xUnit);
+        }
+    }
+    console.log(matrix);
+})();
 
-numbersGenerator();
